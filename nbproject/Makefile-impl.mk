d6c5ad8a37d6dbdcac2c21f080db5873a924dd52
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2020-09-06 19:40:46 +0200

    Pouet

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -27,11 +27,11 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 PROJECTNAME=Empiru
 
 # Active Configuration
-DEFAULTCONF=Debug
+DEFAULTCONF=Debug_Linux
 CONF=${DEFAULTCONF}
 
 # All Configurations
-ALLCONFS=Debug Release Copy_of_Debug 
+ALLCONFS=Debug_Linux Release Debug_Windows 
 
 
 # build
