0ecacb0dd779e96d6284119ab45b8550b6b7338f
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2020-09-06 17:35:07 +0200

    Le PREMIER COMMIIIIIIIIIIIIIIT

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=empiru
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/empiru
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=empiru.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/empiru.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=empiru
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/empiru
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=empiru.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/empiru.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
